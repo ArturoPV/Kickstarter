a0e66fc333918d3b506fb7342fefca5e180bd85d
Arturo <arturopoleo@gmail.com>
2018-03-04 11:52:56 -0400

    Limpiamos codigo de show, y agregamos cache para el metodo volunteersjoined de las iniciativas

--- app/controllers/volunteer_initiatives_controller.rb
+++ app/controllers/volunteer_initiatives_controller.rb
@@ -1,12 +1,20 @@
 class VolunteerInitiativesController < ApplicationController
   def create
     volunteer = VolunteerInitiative.create(user_id: current_user.id, initiative_id: params[:initiative_id])
+
+    initiative = Initiative.find(params[:initiative_id])
+    initiative.increment!(:volunteersjoined, by = 1)
+
     redirect_to initiative_path(params[:initiative_id])
   end
 
   def destroy
     volunteer = VolunteerInitiative.find_by(user_id: current_user.id, initiative_id: params[:initiative_id])
     volunteer.destroy
+
+    initiative = Initiative.find(params[:initiative_id])
+    initiative.decrement!(:volunteersjoined, by = 1)
+
     redirect_to initiative_path(params[:initiative_id])
   end
 end
--- app/models/initiative.rb
+++ app/models/initiative.rb
@@ -3,4 +3,8 @@ class Initiative < ApplicationRecord
   validates :name, :description, :volunteersjoined, :progress, :user_id, presence:  true
   has_many :volunteer_initiatives
   has_many :volunteers, through: :volunteer_initiatives, source: :user
+
+  def has_volunteers?
+    volunteersjoined != 0
+  end
 end
