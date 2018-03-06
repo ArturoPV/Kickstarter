12d302bc1fa76a45fd7b3c27841e1dfa0063eb3c
Arturo <arturopoleo@gmail.com>
2018-03-06 15:24:12 -0400

    Creada index de la pagina

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,9 @@
 class WelcomeController < ApplicationController
+  def index
+    @initiative1 = Initiative.first
+    @initiative2 = Initiative.second
+    @initiative3 = Initiative.third
+    @initiative4 = Initiative.fourth
+    @initiatives = Initiative.first(4)
+  end
 end
