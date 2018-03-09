4611ed92c9e03fc5a1ddf80b319cf09a8ca2d07d
Arturo <arturopoleo@gmail.com>
2018-03-08 22:36:10 -0400

    Fixing Heroku Bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
-  def image_path
-    ActionController::Base.helpers.asset_path("icono_#{rand(1..7)}.jpg")
+  def asset_path_with_fingerprint(asset)
+    ActionController::Base.helpers.asset_path(asset)
+  end
+
+  def initiative_asset_path
+    image_name = "icono_#{rand(1..7)}.jpg"
+    asset_path_with_fingerprint(image_name)
   end
 end
